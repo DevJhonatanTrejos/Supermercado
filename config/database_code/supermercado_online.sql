b136b8c4defa902044be888bfe648ab9aaf467fe
Jhonatan <yhonatanam1998@gmail.com>
2025-11-06 23:40:02 -0500

    bd 06-11-2025

--- config/database_code/supermercado_online.sql
+++ config/database_code/supermercado_online.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2025 a las 23:53:48
+-- Tiempo de generación: 07-11-2025 a las 05:38:39
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,16 +43,19 @@ CREATE TABLE `carrito` (
 CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `descripcion` text DEFAULT NULL
+  `descripcion` text DEFAULT NULL,
+  `imagen_url` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id_categoria`, `nombre`, `descripcion`) VALUES
-(1, 'Granos', 'Granos'),
-(2, 'Limpieza', 'Productos de Aseo, limpieza desinfección');
+INSERT INTO `categoria` (`id_categoria`, `nombre`, `descripcion`, `imagen_url`) VALUES
+(1, 'Granos', 'Granos', 'granos.png'),
+(2, 'Limpieza', 'Productos de Aseo, limpieza desinfección', 'limpieza.png'),
+(3, 'Hogar', 'Productos para el hogar', 'hogar.png'),
+(4, 'Licores', 'Licores, Vinos, etc', 'licores.png');
 
 -- --------------------------------------------------------
 
@@ -120,6 +123,34 @@ CREATE TABLE `envio` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `ofertas`
+--
+
+CREATE TABLE `ofertas` (
+  `id_oferta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `descuento_porcentaje` decimal(5,2) DEFAULT NULL,
+  `precio_oferta` decimal(10,2) DEFAULT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `estado` enum('activa','inactiva','vencida') DEFAULT 'activa',
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha_actualizacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ofertas`
+--
+
+INSERT INTO `ofertas` (`id_oferta`, `id_producto`, `titulo`, `descripcion`, `descuento_porcentaje`, `precio_oferta`, `fecha_inicio`, `fecha_fin`, `estado`, `fecha_creacion`, `fecha_actualizacion`) VALUES
+(1, 6, 'descuento especial jabon Ariel', 'descuento jabones mercadeo ariel', 10.00, 6300.00, '2025-11-01', '2025-11-30', 'activa', '2025-11-07 03:09:27', '2025-11-07 03:09:27'),
+(2, 8, 'descuento en traperos', 'descuento especial en terapeadoras', 10.00, 40500.00, '2025-11-01', '2025-11-13', 'activa', '2025-11-07 03:10:38', '2025-11-07 03:10:38');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pago`
 --
 
@@ -172,7 +203,38 @@ CREATE TABLE `producto` (
 
 INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `stock`, `id_categoria`, `imagen_url`, `estado`) VALUES
 (1, 'Limpido patojito x500', 'Limpido patojito', 2000.00, 10, 2, 'patojito.png', 'disponible'),
-(2, 'arroz diana 500 g', 'arro<', 3000.00, 25, NULL, 'diana_arroz.png', 'disponible');
+(2, 'arroz diana 500 g', 'arroz', 3000.00, 25, 1, 'diana_arroz.png', 'disponible'),
+(3, 'lentejas 500g ', 'lentejas x 500 gramos', 2500.00, 100, 1, 'lenteja.png', 'disponible'),
+(4, 'maiz pira', 'maiz para crispetas', 3450.00, 126, 1, 'maiz.png', 'disponible'),
+(5, 'frijol', 'Frijor cargamanto x 500', 6700.00, 564, 1, 'frijol.png', 'disponible'),
+(6, 'Ariel polvo x 500', 'Jabon ariel en polvo ulta poder', 7000.00, 450, 2, 'ariel.png', 'disponible'),
+(7, 'protex liquido x 500', 'jabon liquido protex x 500 ml avena', 11000.00, 25, 2, 'protex.png', 'disponible'),
+(8, 'trapero', 'trapero con escurridor', 45000.00, 100, 3, 'trapero.png', 'disponible'),
+(9, 'escoba', 'escoba larga', 15000.00, 25, 3, 'escoba.png', 'disponible'),
+(10, 'ron viejo media', 'ron viejo de caldas x 450 ml', 35000.00, 200, 4, 'ron.png', 'disponible'),
+(11, 'vodka', 'vodka absolut 1 litro', 75000.00, 25, 4, 'vodka.png', 'disponible');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_admin`
+--
+
+CREATE TABLE `usuarios_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios_admin`
+--
+
+INSERT INTO `usuarios_admin` (`id_admin`, `nombre`, `usuario`, `clave`, `email`, `creado_en`) VALUES
+(1, 'Administrador Principal', 'admin', '0192023a7bbd73250516f069df18b500', 'admin@supermercadojulio.com', '2025-11-07 04:13:28');
 
 --
 -- Índices para tablas volcadas
@@ -222,6 +284,13 @@ ALTER TABLE `envio`
   ADD KEY `id_pedido` (`id_pedido`);
 
 --
+-- Indices de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD PRIMARY KEY (`id_oferta`),
+  ADD KEY `fk_ofertas_productos` (`id_producto`);
+
+--
 -- Indices de la tabla `pago`
 --
 ALTER TABLE `pago`
@@ -243,6 +312,13 @@ ALTER TABLE `producto`
   ADD KEY `id_categoria` (`id_categoria`);
 
 --
+-- Indices de la tabla `usuarios_admin`
+--
+ALTER TABLE `usuarios_admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -256,7 +332,7 @@ ALTER TABLE `carrito`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
@@ -283,6 +359,12 @@ ALTER TABLE `envio`
   MODIFY `id_envio` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  MODIFY `id_oferta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `pago`
 --
 ALTER TABLE `pago`
@@ -298,7 +380,13 @@ ALTER TABLE `pedido`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_admin`
+--
+ALTER TABLE `usuarios_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -329,6 +417,12 @@ ALTER TABLE `detalle_pedido`
 --
 ALTER TABLE `envio`
   ADD CONSTRAINT `envio_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD CONSTRAINT `fk_ofertas_productos` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `pago`
